d7299484eea0a994fa221ae8c43499459196e5f2
jamjamjam <50306919+feynfeyn888@users.noreply.github.com>
2021-05-03 15:15:36 +0900

    Update main.py

--- Decoder/main.py
+++ Decoder/main.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     # MQTT_PORT : brokerのport
     # MQTT_KEEP_ALIVE : keep alive
     
-    publish(MQTT_HOST="172.16.120.148", MQTT_PORT=1883, MQTT_KEEP_ALIVE=60, query=query)
+    publish(MQTT_HOST="172.16.120.63", MQTT_PORT=1883, MQTT_KEEP_ALIVE=60, query=query)
     print("query:", query)
     
     print("publish")
